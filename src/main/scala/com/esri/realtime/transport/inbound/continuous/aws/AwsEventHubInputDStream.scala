cddc7019afc3deff0f6928483758cfab6d69528a
venukanapartthy <vkanaparthy@esri.com>
2016-08-17 13:32:48 -0700

    added Aws IoT EventHub reciever

--- src/main/scala/com/esri/realtime/transport/inbound/continuous/aws/AwsEventHubInputDStream.scala
+++ src/main/scala/com/esri/realtime/transport/inbound/continuous/aws/AwsEventHubInputDStream.scala
@@ -4,12 +4,18 @@ package com.esri.realtime.transport.inbound.continuous.aws
  *   AwsEventHubInputDStream
  * 
  */
-
+import java.math.BigInteger
 import scala.annotation.meta.param
+import java.security.SecureRandom
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.ReceiverInputDStream
 import org.apache.spark.streaming.receiver.Receiver
+import com.amazonaws.services.iot.client.AWSIotMqttClient
+import com.amazonaws.services.iot.client.AWSIotMessage;
+import com.amazonaws.services.iot.client.AWSIotQos;
+import com.amazonaws.services.iot.client.AWSIotTopic;
+
 
 class AwsEventHubInputDStream(
                            @(transient @param) ssc_ : StreamingContext,
@@ -30,8 +36,50 @@ class AwsEventHubReceiver (clientEndPoint: String,
                            storageLevel: StorageLevel) extends 
         Receiver [String](storageLevel)
                                          {
-  override def onStart(): Unit = ???
-  override def onStop(): Unit = ???  
-  def cleanUp(): Unit = ???
+  private var awsIotClient: AWSIotMqttClient = _
+    
+  override def onStart(): Unit = {
+     setUp()
+  }
+  override def onStop(): Unit = {
+    cleanUp()
+  }
+  
+  def setUp(): Unit = {
+   
+     try {
+          // key-store, password pair
+         val ksp = SslUtil.generateFromFilePath(certificateFile, privateKeyFile)
+         
+         //create aws iot client connection
+         val awsClientId = "clientId-%s".format(new BigInteger(128, new SecureRandom()).toString(32))
+         awsIotClient = new AWSIotMqttClient(clientEndPoint, awsClientId, ksp.keyStore, ksp.keyPass)  
+         
+         //connect to Aws IoT Hub
+         awsIotClient.connect()
+         println("=?Connected to AwsIoT Hub")
+         
+         // subscribe to a topic, register listener, call store on receiving the message
+         awsIotClient.subscribe(new AWSIotTopic(topicName, AWSIotQos.QOS0){
+             override def onMessage(message: AWSIotMessage): Unit = {
+                 store(message.getStringPayload())
+             }      
+           }, true);         
+         println("=>Subscribed to topic: %s".format(topicName))
+
+         
+       }catch{
+         case e: Exception =>
+           restart("Error occured while connecting. Restaring", e)
+       }
+   }
+   
+  def cleanUp(): Unit = {
+    if (awsIotClient !=null) {
+      awsIotClient.disconnect()
+      println("Disconnected from client")
+      awsIotClient = null      
+    }
+  }
   
 }
--- src/main/scala/com/esri/realtime/transport/inbound/continuous/aws/AwsEventHubUtils.scala
+++ src/main/scala/com/esri/realtime/transport/inbound/continuous/aws/AwsEventHubUtils.scala
@@ -1,9 +1,74 @@
 package com.esri.realtime.transport.inbound.continuous.aws
 
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.api.java.{JavaReceiverInputDStream, JavaStreamingContext}
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+
+import scala.reflect.ClassTag
 /*
  * 
- * 
+ * AwsEventHubUtils
  */
 object AwsEventHubUtils {
+  /**
+   * Create an input stream that receives messages published by Aws Event Hub.
+   * @param ssc           StreamingContext object
+   * @param clientEndPoint     Aws Endpoint
+   * @param certificateFile
+   * @param privateKeyFile
+   * @param topicName
+   * @param storageLevel  RDD storage level. Defaults to StorageLevel.MEMORY_AND_DISK_SER_2.
+   */
+  def createStream(
+                    ssc: StreamingContext,
+                    clientEndPoint: String,
+                    certificateFile: String,
+                    privateKeyFile: String,
+                    topicName: String,                     
+                    storageLevel: StorageLevel = StorageLevel.MEMORY_AND_DISK_SER_2
+                    ): ReceiverInputDStream[String] = {
+    new AwsEventHubInputDStream(ssc, clientEndPoint, certificateFile, privateKeyFile, topicName, storageLevel)
+  }
   
+  /**
+   * Create an input stream that receives messages pushed by a Aws Event Hub.
+   * Storage level of the data will be the default StorageLevel.MEMORY_AND_DISK_SER_2.
+   * @param jssc      JavaStreamingContext object
+   * @param clientEndPoint     Aws Endpoint
+   * @param certificateFile
+   * @param privateKeyFile
+   * @param topicName
+   */
+  def createStream(
+                    jssc: JavaStreamingContext,
+                    clientEndPoint: String,
+                    certificateFile: String,
+                    privateKeyFile: String,
+                    topicName: String                               
+                    ): JavaReceiverInputDStream[String] = {
+    implicitly[ClassTag[AnyRef]].asInstanceOf[ClassTag[String]]
+    createStream(jssc.ssc, clientEndPoint, certificateFile, privateKeyFile, topicName)
+  }
+  
+  /**
+   * Create an input stream that receives messages pushed by a Aws Event Hub.
+   * @param jssc      JavaStreamingContext object
+	 * @param clientEndPoint     Aws Endpoint
+   * @param certificateFile
+   * @param privateKeyFile
+   * @param topicName  
+   * @param storageLevel  RDD storage level.
+   */
+  def createStream(
+                    jssc: JavaStreamingContext,
+                    clientEndPoint: String,
+                    certificateFile: String,
+                    privateKeyFile: String,
+                    topicName: String,              
+                    storageLevel: StorageLevel
+                    ): JavaReceiverInputDStream[String] = {
+    implicitly[ClassTag[AnyRef]].asInstanceOf[ClassTag[String]]
+    createStream(jssc.ssc, clientEndPoint, certificateFile, privateKeyFile, topicName, storageLevel)
+  }
 }
